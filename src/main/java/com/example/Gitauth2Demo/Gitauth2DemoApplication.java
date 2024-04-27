dc58fae5687744b1c22eaaa0e7da951f0984a864
anju911j <166814702+anju911j@users.noreply.github.com>
2024-04-27 13:44:41 +0530

    Add files via upload

---
+++ src/main/java/com/example/Gitauth2Demo/Gitauth2DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.Gitauth2Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitauth2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitauth2DemoApplication.class, args);
+		System.out.println("Hello Anjana Here");
+	}
+
+}
---
+++ src/test/java/com/example/Gitauth2Demo/Gitauth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gitauth2Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitauth2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
